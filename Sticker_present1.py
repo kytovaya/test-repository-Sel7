8399a236f42d1bf1db20234ca55c163d831da8f9
Kytovaya Yulya <filippova.yulya@gmail.com>
2017-06-09 21:28:05 +0300

    v2_Задание 8. Сделайте сценарий, проверяющий наличие стикеров у товаров

---
+++ Sticker_present1.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+def are_elements_present(a, *args):
+    return len(a.find_elements(*args))>0
+
+def test_example(driver):
+    driver.get("http://localhost/litecart/en/")
+    images = driver.find_elements_by_css_selector("div.image-wrapper")
+    quantity = len(images)
+    n=0
+    for i in range(quantity):
+        if (len(images[i].find_elements_by_css_selector("div.sticker")) != 1):
+            print("images: ", images[i])
+            n=1
+    if n==0:
+        print("Only one sticker for each image on the page!")
